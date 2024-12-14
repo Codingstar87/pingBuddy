b8ef746f35ec30db349a6a89d8b0429587fc0b71
Codingstar87 <abhijitdatta54321@gmail.com>
2024-12-14 22:30:00 +0530

    update vercel file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:9999/v1/api" : "/v1/api",
+  baseURL: import.meta.env.MODE === "development" ? `${import.meta.env.VITE_BASE_URL}/v1/api` : "/v1/api",
   withCredentials: true,
 });
