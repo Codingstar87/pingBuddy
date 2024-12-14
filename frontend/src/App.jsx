6a22c7c1017c007614409b65ebc1961ea5da9007
Codingstar87 <abhijitdatta54321@gmail.com>
2024-12-14 21:29:19 +0530

    Merged frontend changes from abhi branch into main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import LoginWithOTPage from "./pages/LoginOTPPage";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
@@ -37,12 +38,42 @@ const App = () => {
     <div data-theme={theme}>
       <Navbar />
 
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/login-with-otp" element={!authUser ? <LoginWithOTPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes> */}
+
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login-with-otp"
+          element={!authUser ? <LoginWithOTPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+        {/* Catch-all route for undefined paths */}
+        <Route
+          path="*"
+          element={authUser ? <Navigate to="/" /> : <Navigate to="/login" />}
+        />
       </Routes>
 
       <Toaster />
