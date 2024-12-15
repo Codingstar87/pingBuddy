2b33272464c23d4071849b68b793eab398263ee8
Codingstar87 <abhijitdatta54321@gmail.com>
2024-12-16 00:20:03 +0530

    Fix some error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "https://ping-buddy.vercel.app" : "/v1/api",
-
   withCredentials: true,
 });
