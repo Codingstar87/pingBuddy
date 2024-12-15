433d22c536a671c0e36a350f9190605287bf6b86
Codingstar87 <abhijitdatta54321@gmail.com>
2024-12-16 00:28:08 +0530

    Fix some error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express()
 
 
 app.use(cors({
-    origin :  "https://ping-buddy-yze1.vercel.app" || "*",
+    origin :  "https://ping-buddy-yze1.vercel.app" ,
     credentials : true
 }))
 
