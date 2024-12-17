92443a7d7c6c6772e9cb558a78b55125f239c94c
Codingstar87 <abhijitdatta54321@gmail.com>
2024-12-18 00:25:06 +0530

    24th commit

--- Backend/src/utils/utils.js
+++ Backend/src/utils/utils.js
@@ -10,6 +10,7 @@ const generateToken = (userId, res) => {
 
     res.cookie("jwt", token , {
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        secure: true,                           // Required for HTTPS
         httpOnly : true ,                       //prevent XSS attacts cross-site scripting attacks
         sameSite : "None" ,                     //CSRF  attacks cross-site request forgery attacks
         secure : process.env.NODE_ENV != "development"
