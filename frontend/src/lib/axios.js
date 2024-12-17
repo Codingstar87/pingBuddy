6cca4b3631b61e0f4dea858cb9386d47eb758296
Codingstar87 <abhijitdatta54321@gmail.com>
2024-12-17 23:21:42 +0530

    21th commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+// const a =  import.meta.env.MODE === "development" ? "https://ping-buddy.vercel.app" : "/v1/api"
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://ping-buddy.vercel.app" : "/v1/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://ping-buddy.vercel.app/v1/api" : "/v1/api",
   withCredentials: true,
 });
